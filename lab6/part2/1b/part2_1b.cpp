9c788c8a8fb7ab48d4646b48aae551f09f7bbe2d
pavanvpatil <pavanvpatil01@gmail.com>
2023-02-17 17:58:02 +0530

    corrected sema

--- lab6/part2/1b/part2_1b.cpp
+++ lab6/part2/1b/part2_1b.cpp
@@ -1,41 +1,43 @@
-#include<iostream>
-#include<fstream>
-#include<string>
-#include<vector>
-#include<thread>
-#include<chrono>
-#include<semaphore.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <semaphore.h>
 
 // declare a semaphore
 sem_t sem;
 
 using namespace std;
 
-class pixel {
+class pixel
+{
 public:
-
     int r, g, b;
 
-    pixel(int r, int g, int b) {
+    pixel(int r, int g, int b)
+    {
         this->r = r;
         this->g = g;
         this->b = b;
     }
-
 };
 
-vector<vector<pixel*>> pixels;
+vector<vector<pixel *>> pixels;
 
-// declare a 
+// declare a
 // source: https://www.baeldung.com/cs/convert-rgb-to-grayscale
 //  Luminosity Method
-void grayScale(vector<vector<pixel*>>& pixels) {
+void grayScale(vector<vector<pixel *>> &pixels)
+{
     int width = pixels[0].size();
     int height = pixels.size();
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
 
-           
             sem_wait(&sem);
             float avg = 0.3 * pixels[i][j]->r + 0.59 * pixels[i][j]->g + 0.11 * pixels[i][j]->b;
             avg = (int)avg;
@@ -43,40 +45,42 @@ void grayScale(vector<vector<pixel*>>& pixels) {
             pixels[i][j]->g = avg;
             pixels[i][j]->b = avg;
             sem_post(&sem);
-            
         }
     }
-
 }
 
-void blueTone(vector<vector<pixel*>>& pixels) {
+void blueTone(vector<vector<pixel *>> &pixels)
+{
 
     int width = pixels[0].size();
     int height = pixels.size();
 
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
 
-         
-            sem_wait(&sem);   
+            sem_wait(&sem);
             int b = pixels[i][j]->b;
-            if (b + 50 > 255) {
+            if (b + 50 > 255)
+            {
                 b = 255;
-            } else {
+            }
+            else
+            {
                 b += 50;
             }
             pixels[i][j]->b = b;
             sem_post(&sem);
-            
-
         }
     }
-
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 
-    if(argc != 3) {
+    if (argc != 3)
+    {
         cout << "Usage: ./part1 <input_file> <output_file>" << endl;
         return 1;
     }
@@ -92,36 +96,37 @@ int main(int argc, char* argv[]) {
     string type;
     int width, height, maxColor;
 
-   
-
-    if (in.is_open()) {
+    if (in.is_open())
+    {
 
         in >> type;
         in >> width >> height;
         in >> maxColor;
 
-        pixels.resize(height, vector<pixel*>(width, NULL));
-        
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        pixels.resize(height, vector<pixel *>(width, NULL));
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
                 int r, g, b;
                 in >> r >> g >> b;
                 pixels[i][j] = new pixel(r, g, b);
             }
-        }   
-
+        }
     }
-    
+
     out.open(output_file);
 
-    if (out.is_open()) {
+    if (out.is_open())
+    {
 
         out << type << endl;
         out << width << " " << height << endl;
         out << maxColor << endl;
 
         // init semaphore
-        sem_init(&sem, 0, 0);
+        sem_init(&sem, 0, 1);
 
         thread t1(grayScale, ref(pixels));
         thread t2(blueTone, ref(pixels));
@@ -131,19 +136,20 @@ int main(int argc, char* argv[]) {
         // grayScale(pixels);
         // blueTone(pixels);
 
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
-                out << pixels[i][j]->r << " " << pixels[i][j]->g << " " << pixels[i][j]->b <<" ";
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                out << pixels[i][j]->r << " " << pixels[i][j]->g << " " << pixels[i][j]->b << " ";
             }
             out << endl;
         }
-
     }
 
     out.close();
     in.close();
-    
+
     sem_destroy(&sem);
-    
+
     return 0;
 }
